62198c68cc9da209ded9fa9bfa1dc3fe86bc9c9c
Manan Jain <jainmanan2409@gmail.com>
2025-08-03 10:15:24 +0530

    initial commit for "Stop Watch"

---
+++ Minor Projects/Stop Watch/app.js
@@ -0,0 +1,35 @@
+let startTime = null;
+let elapsedTime = 0;
+let timer = null;
+const display = document.getElementById("display")
+
+function padRead (value) {
+    return String(value).padStart(2, "0")
+}
+
+function updateDisplay() {
+    const minutes = Math.floor(elapsedTime / (1000*60));
+    const seconds = Math.floor(elapsedTime / 1000) % 60;
+    display.innerHTML = `${padRead(minutes)}:${padRead(seconds)}`;
+}
+
+function start() {
+    if(timer) stop()
+    startTime = Date.now() - elapsedTime;
+    timer = setInterval(() => {
+        elapsedTime = Date.now() - startTime;
+        updateDisplay();
+    }, 100)
+    
+} 
+
+function stop() {
+    clearInterval(timer);
+}
+
+function reset() {
+    stop()
+    elapsedTime = 0;
+    updateDisplay()
+}
+
